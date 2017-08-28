37511ee41095d3cdc471bbc8529a3cba0cfade29
Daniel Lim <ddanielnp@hotmail.com>
2017-08-28 13:36:28 +0800

    commit for gh-pages

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+
+  componentDidMount () {
+    const url = 'https://glacial-hollows-14757.herokuapp.com/articles'
+    fetch(url)
+    .then(function (response) {
+      return response.json
+    })
+    .then(function (json) {
+      console.log('parsed json', json)
+    })
+    .catch(function (ex) {
+      console.error('error', ex)
+    })
+  }
+
+}
+
+export default App
